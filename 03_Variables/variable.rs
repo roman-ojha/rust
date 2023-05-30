a09f7705e6f3d579c44d099f35d68ca24d683043
Roman-Ojha <razzroman98@gmail.com>
2023-05-30 22:34:01 +0545

    docs: data types & variables

---
+++ 03_Variables/variable.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    let two = 2; // variable store integer value
+    let hello = "hello"; // variable store string value
+    let j = 'j';
+    let my_half = 0.5;
+    let mut my_name = "Roman"; // this is a mutable variable so, it means that we can change the value of this variable later
+    let quit_program = false;
+    let your_half = my_half; // assigning different variable value 0.5
+}
+
